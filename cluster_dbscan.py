6a5958f3e7e9117ba3c137e0732db194f4c60e99
Nikita Kryukov <cravtos92@gmail.com>
2021-07-25 23:48:38 +0700

    Remove sessions with less than min_req requests
    * Add flag for RPI interval length

--- cluster_dbscan.py
+++ cluster_dbscan.py
@@ -1,39 +1,39 @@
 import argparse
 import ast
 
-import sklearn
 from sklearn.cluster import DBSCAN
-from sklearn.preprocessing import StandardScaler
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 
-def clustering(csv_file: str) -> None:
+def clustering(csv_file: str, int_len: int, min_req: int) -> None:
     """
     Cluster clients using RPI and deviation. Plot result.
+    :param int_len: Length of interval in RPI
+    :param min_req: Minimal amount of request done by client to be considered for clustering
     :param csv_file: CSV file containing RPI and Deviation columns
     :return: None
     """
 
-    # TODO: don't hardcode interval length
     requests = pd.read_csv(
         csv_file,
         dtype={"Deviation": np.float64},
-        converters={"RPI30": ast.literal_eval},
+        converters={f"RPI{int_len}": ast.literal_eval},
     )
 
+    # Remove rows with None values
     requests = requests.dropna()
 
-    deviations = requests["Deviation"]
-    rpis = [rpi[0] for rpi in requests["RPI30"]]
+    # Remove sessions with less than 5 requests
+    requests = requests[requests["Diff"].apply(lambda x: len(x) + 1 >= min_req)]
 
+    deviations = requests["Deviation"]
+    rpis = [rpi[0] for rpi in requests[f"RPI{int_len}"]]
     data = np.array([[rpi, deviation] for rpi, deviation in zip(rpis, deviations)])
 
-    # NOTE: value for alg with normalization: eps = 0.5
     # data = sklearn.preprocessing.StandardScaler().fit_transform(data)
-
     db = DBSCAN(eps=10, min_samples=4, metric="euclidean").fit(data)
 
     labels = db.labels_
@@ -42,11 +42,12 @@ def clustering(csv_file: str) -> None:
 
     print("Estimated number of clusters: %d" % n_clusters_)
     print("Estimated number of noise points: %d" % n_noise_)
-    print("Estimated number of points: %d" % len(data))
+    print("Number of points: %d" % len(data))
 
+    # Write client, data for clustering and its label to ./dumps/labeling.csv
     requests = requests[["IP", "UA", "Session"]]
     requests["Label"] = labels
-    requests["RPI30"] = rpis
+    requests[f"RPI{int_len}"] = rpis
     requests["Deviation"] = deviations
     requests.to_csv("./dumps/labeling.csv", index=False)
 
@@ -83,7 +84,7 @@ def clustering(csv_file: str) -> None:
             markersize=4,
         )
 
-    plt.xlabel("RPI")
+    plt.xlabel(f"RPI{int_len}")
     plt.ylabel("Mean deviation")
     plt.title("Estimated number of clusters: %d" % n_clusters_)
     plt.show()
@@ -98,6 +99,22 @@ if __name__ == "__main__":
         help="CSV file with RPI and Deviation columns",
         default="./dumps/requests.csv",
     )
+    parser.add_argument(
+        "--interval-length",
+        dest="int_len",
+        metavar="l",
+        type=int,
+        help="Length of interval for RPI column",
+        default=30,
+    )
+    parser.add_argument(
+        "--min-req",
+        dest="min_req",
+        metavar="m",
+        type=int,
+        help="Minimal amount of requests done by client in one session to be considered for clustering",
+        default=5,
+    )
 
     args = parser.parse_args()
-    clustering(args.input)
+    clustering(args.input, args.int_len, args.min_req)
