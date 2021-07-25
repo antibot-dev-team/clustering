835b6de276a5f6dcdac14f5ea281f5883b929f44
Nikita Kryukov <cravtos92@gmail.com>
2021-07-25 18:20:23 +0700

    Store results of parsing in csv file (#3)
    * Use csv file for interval parsing
    * Add .idea/ to .gitignore
    * Output to csv file after deviation parsing
    * Add dumps/*.csv to .gitignore
    * Update requirements.txt
    * Use pandas with clustering
    * Read data for clustering from csv file
    * Write csv file with client and its label

--- cluster_dbscan.py
+++ cluster_dbscan.py
@@ -1,110 +1,104 @@
+import argparse
+import ast
+
 import sklearn
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-import argparse
-import json
+import pandas as pd
 
 
-def clustering(loginterval: str, logdeviation: str) -> None:
+def clustering(csv_file: str) -> None:
     """
     Cluster clients using RPI and deviation. Plot result.
-    :param loginterval: JSON of the form {"IP:UA": [amount of requests per intervals]}
-    :param logdeviation: JSON of the form {"IP:UA": int(standard deviation)}
+    :param csv_file: CSV file containing RPI and Deviation columns
     :return: None
     """
 
-    with open(loginterval, "r") as interfile:
-        data_interval = json.load(interfile)
+    # TODO: don't hardcode interval length
+    requests = pd.read_csv(csv_file, converters={"RPI30": ast.literal_eval,
+                                                 "Deviation": ast.literal_eval,
+                                                 "Diff": ast.literal_eval})
 
-    with open(logdeviation, "r") as interfile:
-        data_deviation = json.load(interfile)
+    requests = requests[requests["Deviation"].map(lambda x: x[0] is not None)]
 
-    for key, value in data_interval.items():
-        print(key, "\t", len(value), "\t", value)
+    deviations = [deviation[0] for deviation in requests["Deviation"].tolist()]
+    rpis = [rpi[0][0] for rpi in requests["RPI30"].tolist()]
 
-    # data = np.array(
-    #     [
-    #         [intervals[0], deviation]
-    #         for intervals, deviation in zip(
-    #             data_interval.values(), data_deviation.values()
-    #         )
-    #     ]
-    # )
+    data = np.array([
+        [rpi, deviation]
+        for rpi, deviation in zip(rpis, deviations)]
+    )
 
-    # # NOTE: value for alg with normalization: eps = 0.5
+    # NOTE: value for alg with normalization: eps = 0.5
     # data = sklearn.preprocessing.StandardScaler().fit_transform(data)
-    #
-    # db = DBSCAN(eps=0.5, min_samples=4, metric="euclidean").fit(data)
-    #
-    # labels = db.labels_
-    # n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    # n_noise_ = list(labels).count(-1)
-    #
-    # print("Estimated number of clusters: %d" % n_clusters_)
-    # print("Estimated number of noise points: %d" % n_noise_)
-    # print("Estimated number of points: %d" % len(data))
-    #
-    # # pprint(dict(zip(data_interval.keys(), labels)))
-    #
-    # # Draw
-    # core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-    # core_samples_mask[db.core_sample_indices_] = True
-    #
-    # unique_labels = set(labels)
-    # colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
-    # for k, col in zip(unique_labels, colors):
-    #     if k == -1:
-    #         # Black used for noise.
-    #         col = [0, 0, 0, 1]
-    #
-    #     class_member_mask = labels == k
-    #
-    #     xy = data[class_member_mask & core_samples_mask]
-    #     plt.plot(
-    #         xy[:, 0],
-    #         xy[:, 1],
-    #         "o",
-    #         markerfacecolor=tuple(col),
-    #         markeredgecolor="k",
-    #         markersize=4,
-    #     )
-    #
-    #     xy = data[class_member_mask & ~core_samples_mask]
-    #     plt.plot(
-    #         xy[:, 0],
-    #         xy[:, 1],
-    #         "o",
-    #         markerfacecolor=tuple(col),
-    #         markeredgecolor="k",
-    #         markersize=4,
-    #     )
-    #
-    # plt.xlabel("RPI")
-    # plt.ylabel("Mean deviation")
-    # plt.title("Estimated number of clusters: %d" % n_clusters_)
-    # plt.show()
+
+    db = DBSCAN(eps=10, min_samples=4, metric="euclidean").fit(data)
+
+    labels = db.labels_
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+
+    print("Estimated number of clusters: %d" % n_clusters_)
+    print("Estimated number of noise points: %d" % n_noise_)
+    print("Estimated number of points: %d" % len(data))
+
+    requests = requests[["IP", "UA"]]
+    requests["Label"] = labels
+    requests["RPI30"] = rpis
+    requests["Deviation"] = deviations
+    requests.to_csv("./dumps/labeling.csv", index=False)
+
+    # Draw
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+
+        class_member_mask = labels == k
+
+        xy = data[class_member_mask & core_samples_mask]
+        plt.plot(
+            xy[:, 0],
+            xy[:, 1],
+            "o",
+            markerfacecolor=tuple(col),
+            markeredgecolor="k",
+            markersize=4,
+        )
+
+        xy = data[class_member_mask & ~core_samples_mask]
+        plt.plot(
+            xy[:, 0],
+            xy[:, 1],
+            "o",
+            markerfacecolor=tuple(col),
+            markeredgecolor="k",
+            markersize=4,
+        )
+
+    plt.xlabel("RPI")
+    plt.ylabel("Mean deviation")
+    plt.title("Estimated number of clusters: %d" % n_clusters_)
+    plt.show()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Cluster using DBSCAN algorithm.")
     parser.add_argument(
-        "--rpi",
-        metavar="r",
+        "--input",
+        metavar="i",
         type=str,
-        help="JSON with the amount of requests per given intervals",
-        default="./dumps/log_clients_30s_100k.json",
-    )
-    parser.add_argument(
-        "--deviation",
-        metavar="d",
-        type=str,
-        help="JSON with the standard deviation for rpi",
-        default="./dumps/log_clients_deviation_100k.json",
+        help="CSV file with RPI and Deviation columns",
+        default="./dumps/requests.csv",
     )
 
     args = parser.parse_args()
-    clustering(args.rpi, args.deviation)
+    clustering(args.input)
