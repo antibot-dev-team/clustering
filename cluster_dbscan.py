a13ec1261e1b2c6b3abb9d7dddfc27c413a97b1c
alskv <alskv13@gmail.com>
2021-07-26 19:07:00 +0700

    Update clustering. * Add a one-dimensional case. * Add flags. * Add a dump of top-100 clients.

--- cluster_dbscan.py
+++ cluster_dbscan.py
@@ -1,3 +1,4 @@
+from numpy import array
 import argparse
 import ast
 
@@ -8,33 +9,33 @@ import numpy as np
 import pandas as pd
 
 
-def clustering(csv_file: str, eps: int, min_samples: int, int_len: int, min_req: int) -> None:
+def two_dim_clustering(
+    csv_file: str, usg_data: str, int_len: int, min_req: int, eps: int, min_samples: int
+) -> None:
     """
-    Cluster clients using RPI and deviation. Plot result.
-
-    :param min_samples: Minimal samples parameter for DBSCAN
-    :param eps: Epsilon parameter for DBSCAN
+    Two-dimensional cluster clients using RPI and deviation or mean. Plot result.
+    :param csv_file: CSV file containing RPI and Deviation columns
+    :param usg_data: Data for clustering. Available: Deviation, Mean
     :param int_len: Length of interval in RPI
     :param min_req: Minimal amount of request done by client to be considered for clustering
-    :param csv_file: CSV file containing RPI and Deviation columns
     :return: None
     """
 
     requests = pd.read_csv(
         csv_file,
-        dtype={"Deviation": np.float64},
-        converters={f"RPI{int_len}": ast.literal_eval}
+        dtype={f"{usg_data}": np.float64},
+        converters={f"RPI{int_len}": ast.literal_eval},
     )
 
     # Remove rows with None values
-    requests = requests.dropna()
+    requests = requests.dropna(axis=0, how="any")
 
     # Remove sessions with less than 5 requests
     requests = requests[requests["Diff"].apply(lambda x: len(x) + 1 >= min_req)]
 
-    deviations = requests["Deviation"]
+    second_param = requests[f"{usg_data}"]
     rpis = [rpi[0] for rpi in requests[f"RPI{int_len}"]]
-    data = np.array([[rpi, deviation] for rpi, deviation in zip(rpis, deviations)])
+    data = np.array([[rpi, deviation] for rpi, deviation in zip(rpis, second_param)])
 
     # data = sklearn.preprocessing.StandardScaler().fit_transform(data)
     db = DBSCAN(eps=eps, min_samples=min_samples, metric="euclidean").fit(data)
@@ -43,16 +44,24 @@ def clustering(csv_file: str, eps: int, min_samples: int, int_len: int, min_req:
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
     n_noise_ = list(labels).count(-1)
 
+    print(f"=== 2d clustering for RPI{int_len} and {usg_data} ===")
     print("Estimated number of clusters: %d" % n_clusters_)
     print("Estimated number of noise points: %d" % n_noise_)
-    print("Number of points: %d" % len(data))
+    print("Number of points: %d\n" % len(data))
 
     # Write client, data for clustering and its label to ./dumps/labeling.csv
     requests = requests[["IP", "UA", "Session"]]
     requests["Label"] = labels
     requests[f"RPI{int_len}"] = rpis
-    requests["Deviation"] = deviations
-    requests.to_csv("./dumps/labeling.csv", index=False)
+    requests[f"{usg_data}"] = second_param
+    requests.to_csv("./dumps/2d_labeling.csv", index=False)
+
+    # Dump top-100 interesting clients
+    sort = requests.sort_values(by=f"RPI{int_len}", ascending=False)
+    sort[:100].to_csv(f"./dumps/top_most_RPI{int_len}.csv", index=False)
+
+    sort = sort.sort_values(by=f"{usg_data}", ascending=True)
+    sort[:100].to_csv(f"./dumps/top_least_{usg_data}.csv", index=False)
 
     # Draw
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
@@ -74,7 +83,7 @@ def clustering(csv_file: str, eps: int, min_samples: int, int_len: int, min_req:
             "o",
             markerfacecolor=tuple(col),
             markeredgecolor="k",
-            markersize=4,
+            markersize=8,
         )
 
         xy = data[class_member_mask & ~core_samples_mask]
@@ -84,11 +93,72 @@ def clustering(csv_file: str, eps: int, min_samples: int, int_len: int, min_req:
             "o",
             markerfacecolor=tuple(col),
             markeredgecolor="k",
-            markersize=4,
+            markersize=6,
         )
 
     plt.xlabel(f"RPI{int_len}")
-    plt.ylabel("Mean deviation")
+    plt.ylabel(f"{usg_data}")
+    plt.title("Estimated number of clusters: %d" % n_clusters_)
+    plt.show()
+
+
+def one_dim_clustering(
+    csv_file: str, usg_data: str, int_len: int, min_req: int, eps: int, min_samples: int
+) -> None:
+    """
+    One-dimensional cluster clients using RPI or deviation or mean. Plot result.
+    :param csv_file: CSV file containing RPI and Deviation columns
+    :param usg_data: Data for clustering. Available: RPI{int_lеn}, Deviation, Mean
+    :param int_len: Length of interval in RPI
+    :param min_req: Minimal amount of request done by client to be considered for clustering
+    :return: None
+    """
+
+    # Converters for different types of data: RPI is str->list, Mean and Dev is a float64
+    converters = (
+        {f"{usg_data}": ast.literal_eval} if f"{usg_data}" == f"RPI{int_len}" else None
+    )
+    requests = pd.read_csv(csv_file, converters=converters)
+    requests = requests.dropna()
+    requests = requests[requests["Diff"].apply(lambda x: len(x) + 1 >= min_req)]
+
+    datas = array(
+        [
+            data[0] if f"{usg_data}" == f"RPI{int_len}" else data
+            for data in requests[f"{usg_data}"]
+        ]
+    ).reshape(-1, 1)
+
+    db = DBSCAN(eps=eps, min_samples=min_samples, metric="euclidean").fit(datas)
+
+    labels = db.labels_
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+
+    print(f"=== 1d clustering for {usg_data} ===")
+    print("Estimated number of clusters: %d" % n_clusters_)
+    print("Estimated number of noise points: %d" % n_noise_)
+    print("Estimated number of points: %d" % len(datas))
+
+    requests = requests[["IP", "UA"]]
+    requests["Label"] = labels
+    requests[f"{usg_data}"] = datas
+    requests.to_csv("./dumps/1d_labeling.csv", index=False)
+
+    sort = requests.sort_values(by=f"{usg_data}", ascending=False)
+    sort[:100].to_csv(f"./dumps/top_most_{usg_data}_1d.csv", index=False)
+
+    sort = requests.sort_values(by=f"{usg_data}", ascending=True)
+    sort[:100].to_csv(f"./dumps/top_least_{usg_data}_1d.csv", index=False)
+
+    # Draw
+    # plt.figure(figsize=(12, 7))
+    plt.xticks(np.arange(-1, n_clusters_, step=1))
+    plt.plot(labels, datas, "o-r", alpha=0.7, lw=0, mec="b", mew=1, ms=10)
+
+    plt.grid(True)
+    plt.xlabel("Clusters №")
+    plt.ylabel(f"{usg_data}")
     plt.title("Estimated number of clusters: %d" % n_clusters_)
     plt.show()
 
@@ -103,6 +173,14 @@ if __name__ == "__main__":
         default="./dumps/requests.csv",
     )
     parser.add_argument(
+        "--dimensionality",
+        dest="dim",
+        metavar="d",
+        type=int,
+        help="Data dimension for clustering. Available: 1, 2",
+        default=0,
+    )
+    parser.add_argument(
         "--interval-length",
         dest="int_len",
         metavar="l",
@@ -111,20 +189,36 @@ if __name__ == "__main__":
         default=30,
     )
     parser.add_argument(
+        "--one-date",
+        dest="one_dim_date",
+        metavar="1d",
+        type=str,
+        help="Data from CSV for 1D clustering. Available: RPI{},Deviation,Mean",
+        default="Mean",
+    )
+    parser.add_argument(
+        "--two-date",
+        dest="two_dim_date",
+        metavar="2d",
+        type=str,
+        help="Second data from CSV for 2D clustering. Available: Deviation,Mean",
+        default="Deviation",
+    )
+    parser.add_argument(
         "--min-req",
         dest="min_req",
-        metavar="mr",
+        metavar="m",
         type=int,
         help="Minimal amount of requests done by client in one session to be considered for clustering",
-        default=5,
+        default=4,
     )
     parser.add_argument(
         "--eps",
-        dest="e",
+        dest="eps",
         metavar="e",
         type=int,
         help="Epsilon parameter for dbscan",
-        default=5,
+        default=10,
     )
     parser.add_argument(
         "--min-samples",
@@ -136,4 +230,38 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    clustering(args.input, args.e, args.min_samples, args.int_len, args.min_req)
+    if args.dim == 1:
+        one_dim_clustering(
+            args.input,
+            args.one_dim_date,
+            args.int_len,
+            args.min_req,
+            args.eps,
+            args.min_samples,
+        )
+    elif args.dim == 2:
+        two_dim_clustering(
+            args.input,
+            args.two_dim_date,
+            args.int_len,
+            args.min_req,
+            args.eps,
+            args.min_samples,
+        )
+    else:
+        one_dim_clustering(
+            args.input,
+            args.one_dim_date,
+            args.int_len,
+            args.min_req,
+            args.eps,
+            args.min_samples,
+        )
+        two_dim_clustering(
+            args.input,
+            args.two_dim_date,
+            args.int_len,
+            args.min_req,
+            args.eps,
+            args.min_samples,
+        )
