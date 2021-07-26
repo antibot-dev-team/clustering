44cdb6cf1e2014c7d074bf84694872949c8e45f4
Nikita Kryukov <cravtos92@gmail.com>
2021-07-26 12:00:05 +0700

    Combine parse_interval.py and parse_deviation.py into parse_log.py
    There was an issue that result of "parse_interval.py" contained in total more sessions, than result of "parse_deviation.py" did. Turned out it was because of different code in two files for parsing client requests to sessions.

--- parse_log.py
+++ parse_log.py
@@ -2,7 +2,6 @@ import datetime
 import argparse
 import re
 import math
-import time
 import os
 
 from collections import defaultdict
@@ -10,20 +9,23 @@ from collections import defaultdict
 import pandas as pd
 
 
-def parse_deviation(log_name: str, limit=0) -> None:
+def parse_deviation(log_name: str, interval: int, limit=0) -> None:
     """
     Create or append columns to ./dumps/requests.csv file:
-    1) Time difference between requests
-    2) Mean value of the differences between requests
-    3) Mean deviation of time difference between requests
+    1) Time difference between requests in each session
+    2) Mean value of the differences between requests in each session
+    3) Mean deviation of time difference between requests in each session
+    4) Requests per interval for each session
 
+    :param interval: Duration of intervals for calculating RPI
     :param log_name: Name of file with logs
     :param limit: Amount of lines to consider
     :return: None
     """
 
     # Parse client requests
-    clients_reqs = dict()
+    clients_reqs = defaultdict(list)  # Will contain all sessions
+    clients_session_reqs = defaultdict(list)  # Temp dict for filling sessions
     with open(log_name, "r") as log_file:
         i = 0
         for line in log_file:
@@ -38,37 +40,65 @@ def parse_deviation(log_name: str, limit=0) -> None:
             ts = datetime.datetime.strptime(
                 ts[0], "%d/%b/%Y:%H:%M:%S %z"
             )  # e.g. [22/Jan/2019:06:38:40 +0330]
-            ts = time.mktime(ts.timetuple())
+            ts = ts.timestamp()
 
             client = "{}:{}".format(ip, ua)
-            if client not in clients_reqs:
-                clients_reqs[client] = list()
-            clients_reqs[client].append(ts)
+            if (
+                    len(clients_session_reqs[client]) > 0
+                    and ts - clients_session_reqs[client][-1] >= 30 * 60
+            ):
+                clients_reqs[client].append(clients_session_reqs[client])
+                clients_session_reqs[client] = []
+
+            clients_session_reqs[client].append(ts)
+
+    # Append last session to the dict
+    for client, requests in clients_session_reqs.items():
+        if len(requests) > 0:
+            clients_reqs[client].append(requests)
+
+    # Calculate RPI for each interval
+    client_rpi = defaultdict(list)
+    for client, requests in clients_reqs.items():
+        request_speed = []
+        for session in requests:
+            session_speed = []
+            start_ts = session[0]
+            interval_reqs = 0
+            for ts in session:
+                if ts - start_ts >= interval:
+                    session_speed.append(interval_reqs / interval)
+                    start_ts = ts
+                    interval_reqs = 0
+                interval_reqs += 1
+                if ts is session[-1]:
+                    session_speed.append(interval_reqs / interval)
+            request_speed.append(session_speed)
+        client_rpi[client] = request_speed
 
     # Calculate differences between neighbour timestamps
     clients_diff = defaultdict(list)
-    for client, timestamps in clients_reqs.items():
-        sessions_centred_diffs = []
-        for i in range(len(timestamps) - 1):
-            diff = timestamps[i + 1] - timestamps[i]
-            if diff < 60 * 30:  # less than 30 minutes difference
-                sessions_centred_diffs.append(diff)
-            else:
-                clients_diff[client].append(sessions_centred_diffs if len(sessions_centred_diffs) > 0 else None)
-                sessions_centred_diffs = []
+    for client, sessions in clients_reqs.items():
+        session_diffs = []
+        for session in sessions:
+            for i in range(len(session) - 1):
+                diff = session[i + 1] - session[i]
+                if diff < 60 * 30:  # less than 30 minutes difference
+                    session_diffs.append(diff)
+                else:
+                    clients_diff[client].append(session_diffs if len(session_diffs) > 0 else None)
+                    session_diffs = []
 
-        if len(sessions_centred_diffs) > 0:  # all timestamps was read but session is not finished
-            clients_diff[client].append(sessions_centred_diffs)
-        elif len(timestamps) == 1:  # only one timestamp (diff can't be calculated for this session)
-            clients_diff[client].append(None)
+            if len(session_diffs) > 0:  # all timestamps was read but session is not finished
+                clients_diff[client].append(session_diffs)
+            elif len(session) == 1:  # only one timestamp (diff can't be calculated for this session)
+                clients_diff[client].append(None)
 
     # Calculate mean of differences
     clients_mean = defaultdict(list)
     for client in clients_diff:
         for session in clients_diff[client]:
             if session is not None:
-                if len(session) == 0:
-                    print(f"{client}, {session}, {clients_diff[client]}")
                 clients_mean[client].append(sum(session) / len(session))
             else:
                 clients_mean[client].append(None)
@@ -102,17 +132,22 @@ def parse_deviation(log_name: str, limit=0) -> None:
             )
 
     # Write to csv
+    df_rpi = dict_to_df(client_rpi, f"RPI{interval}")
     df_diff = dict_to_df(clients_diff, "Diff")
     df_mean = dict_to_df(clients_mean, "Mean")
     df_deviation = dict_to_df(clients_deviation, "Deviation")
 
-    df = pd.merge(df_diff, df_mean, on=["IP", "UA", "Session"])
+    df = pd.merge(df_rpi, df_diff, on=["IP", "UA", "Session"])
+    df = df.merge(df_mean, on=["IP", "UA", "Session"])
     df = df.merge(df_deviation, on=["IP", "UA", "Session"])
 
     if os.path.isfile("./dumps/requests.csv"):
         df_old = pd.read_csv("./dumps/requests.csv")
         if "Diff" in df_old.columns:
             df_old = df_old.drop("Diff", axis=1)
+
+        if f"RPI{interval}" in df_old.columns:
+            df_old = df_old.drop(f"RPI{interval}", axis=1)
 
         if "Mean" in df_old.columns:
             df_old = df_old.drop("Mean", axis=1)
@@ -150,7 +185,8 @@ def dict_to_df(dictionary: dict, col_name: str) -> pd.DataFrame:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Calculate requests per interval and dump to json file."
+        description="Calculate RPI, request time differences, "
+                    "their mean and std deviation and write it to ./dumps/requests.csv"
     )
     parser.add_argument(
         "--log", metavar="f", type=str, help="Log file", default="./access.log"
@@ -162,6 +198,13 @@ if __name__ == "__main__":
         help="Parse specified amount of lines.",
         default=100_000,
     )
+    parser.add_argument(
+        "--interval",
+        metavar="t",
+        type=int,
+        help="Interval duration in seconds.",
+        default=30,
+    )
     args = parser.parse_args()
 
-    parse_deviation(args.log, args.limit)
+    parse_deviation(args.log, args.interval, args.limit)
